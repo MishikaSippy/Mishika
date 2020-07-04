b48ec2ed46fc701abe86965cef77e37deab684ee
MishikaSippy <66460940+MishikaSippy@users.noreply.github.com>
2020-07-04 16:05:09 +0530

    Update image_blurring_with_cursor.py

--- image_blurring_with_cursor.py
+++ image_blurring_with_cursor.py
@@ -1,36 +1,34 @@
 import cv2
 import numpy as np
 
-img=cv2.imread('quotes.jpg')
-wname='Mishika'
+img = cv2.imread('quotes.jpg')
+wname = 'Mishika'
 cv2.namedWindow(wname)
 
-abc=False
+abc = False
 
 def draw_eraser(event,x,y,flags,param):
     global abc
-    if event==cv2.EVENT_LBUTTONDOWN:
+    if event == cv2.EVENT_LBUTTONDOWN:
         abc = True
         #cv2.circle(img,(x,y),30,(255,255,255),-1)
-    elif event==cv2.EVENT_MOUSEMOVE:
-        if abc==True:
-           sub_face =img[y:y+30,x:x+30]
-           sub_face =cv2.GaussianBlur(sub_face,(23,23),30)
-           img[y:y+sub_face.shape[0],x:x+sub_face.shape[1]]=sub_face
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if abc == True:
+           blur = img[y:y+30,x:x+30]
+           blur = cv2.GaussianBlur(blur,(23,23),30)
+           img[y:y+blur.shape[0],x:x+blur.shape[1]]=blur
         else :
-            abc=False
+            abc = False
 cv2.setMouseCallback(wname,draw_eraser)
 
-img[0:30,:]=0
-img[:,0:30]=0
-img[450:480,:]=0
-img[:,650:680]=0
-
-
+img[0:30,:] = 0
+img[:,0:30] = 0
+img[450:480,:] = 0
+img[:,650:680] = 0
 
 while True:
     cv2.imshow(wname,img)
-    if cv2.waitKey(1)==ord('q'):
+    if cv2.waitKey(1) == ord('q'):
         break
 
 cv2.destroyAllWindows()
